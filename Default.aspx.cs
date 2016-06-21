2ab62746a2c223adcaefe646f85c83ce3e6507d0
nilesh_thakkar <nilesh.mscit@gmail.com>
2016-06-21 20:07:27 +0400

    Fixed

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -24,15 +24,65 @@ public partial class _Default : System.Web.UI.Page
 
             MailAddress fromAddress = new MailAddress("info@suguae.net");
             oMail.From = fromAddress;
+            oMail.To.Add(userEmail);
+            if (isCC)
+            {
+                oMail.Bcc.Add("nilesh.mscit@gmail.com");
+                oMail.Bcc.Add("pradsir@gmail.com");
+                oMail.Bcc.Add("ashish.bansal9@yahoo.com");
+            }
+
+            oMail.Subject = "SUGUAE Registration";
+            oMail.IsBodyHtml = true;
+            oMail.Priority = MailPriority.High;
+
+            oMail.IsBodyHtml = true;
+            oMail.Body = "Thank you for registering with SUGUAE! <br/> We will get back to you. <br/><br/> SUGUAE";
+
+            SmtpMail.Host = SERVER;
+            SmtpMail.Send(oMail);
+            oMail = null;	// free up resources
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    protected void btnSubscribe_ServerClick(object sender, EventArgs e)
+    {
+        sendEmailToUs(string.Empty, txtEmailSubscribe.Value, true);
+        sendEmail(txtEmailSubscribe.Value, txtEmailSubscribe.Value, true);
+    }
+
+    protected void btnRegister_ServerClick(object sender, EventArgs e)
+    {        
+        sendEmail(txtEmail.Value, txtEmail.Value, true);
+    }
+
+
+
+    private bool sendEmailToUs(string userEmail, string msgBody, bool isCC)
+    {
+        try
+        {
+            const string SERVER = "relay-hosting.secureserver.net";
+            SmtpClient SmtpMail = new SmtpClient();
+            MailMessage oMail = new MailMessage();
+
+            MailAddress fromAddress = new MailAddress("info@suguae.net");
+            oMail.From = fromAddress;
             oMail.To.Add("suguae@yahoo.com");
             if (isCC)
             {
-                oMail.CC.Add("nilesh.mscit@gmail.com");
-                oMail.CC.Add("pradsir@gmail.com");
-                oMail.CC.Add("ashish.bansal9@yahoo.com");
+                oMail.Bcc.Add("nilesh.mscit@gmail.com");
+                oMail.Bcc.Add("pradsir@gmail.com");
+                oMail.Bcc.Add("ashish.bansal9@yahoo.com");
             }
 
-            oMail.Subject = "Thanks for contacting SUGUAE!";
+            oMail.Subject = "Somebody registered with SUGUAE";
             oMail.IsBodyHtml = true;
             oMail.Priority = MailPriority.High;
 
@@ -49,15 +99,5 @@ public partial class _Default : System.Web.UI.Page
         {
             return false;
         }
-    }
-
-    protected void btnSubscribe_ServerClick(object sender, EventArgs e)
-    {
-        sendEmail(string.Empty, txtEmailSubscribe.Value, true);
-    }
-
-    protected void btnRegister_ServerClick(object sender, EventArgs e)
-    {
-        sendEmail(txtEmail.Value, txtName.Value, true);
     }
 }
